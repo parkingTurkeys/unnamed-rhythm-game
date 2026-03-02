3d5872de1e41c2310f1bf078605be9b0d66f93ea
parking_turkeys <p-t@hackclub.app>
2026-03-02 17:29:28 -0500

    I HATE php

--- login.gd
+++ login.gd
@@ -15,14 +15,16 @@ func _on_submit_pressed() -> void:
 	$HTTPRequest.request_completed.connect(_on_request_completed)
 	# $HTTPRequest.request(GlobalData.api_route + "login.php")
 	var url = GlobalData.api_route + "login.php/"
-	var data_to_send = "{'username':'" + %name_input.text + "','password':'" + %password_input.text + "'}"
-	var json = JSON.stringify(data_to_send)
-	var headers = ["Content-Type: application/json"]
-	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, json)
+	print('{"username":"' + %name_input.text + '","password":"' + %password_input.text + '"}')
+	var data_to_send = "username=" + %name_input.text + "&password=" + %password_input.text
+	# var json = JSON.stringify(data_to_send)
+	# print(json)
+	var headers = ["Content-Type: application/x-www-form-urlencoded"]
+	$HTTPRequest.request(GlobalData.api_route + "login.php/",headers, HTTPClient.METHOD_POST, data_to_send)
 
 func _on_request_completed(result, response_code, headers, body):
 		print(body.get_string_from_utf8())
-		var json = JSON.parse_string('{"success":false}')
+		var json = JSON.parse_string(body.get_string_from_utf8())
 		if (json["success"]):
 			print(json["id"])
 		else:
