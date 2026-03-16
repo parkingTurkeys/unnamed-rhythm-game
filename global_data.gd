442cbae90672087923ff5fd5bf902b9bfcba6857
parking_turkeys <p-t@hackclub.app>
2026-03-15 20:39:51 -0400

    notes are being checked :3]

--- global_data.gd
+++ global_data.gd
@@ -6,3 +6,4 @@ var api_route = "http://localhost:9000/"
 
 var NOTE_SPEED = 6
 var current_note = ""
+var next_note = ""
--- rhythming.gd
+++ rhythming.gd
@@ -2,17 +2,21 @@ extends Node2D
 # later, this will be a user:// path probably :3
 var map_loc = "res://maps/test.txt"
 @onready var metronome = %metronome
+@onready var acc_timer = %acc_timeout
 var actions = ["h", "j", "k", "l", "a", "d", "w", "s"]
 var beatmap
 var note = preload("res://rhythm_display.tscn")
-var clicks = []
+var all_clicks = []
+var current_note_clicks = []
+var current_acc_clicks = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	beatmap = file_to_array(map_loc)
 	print(beatmap)
-	metronome.start(60.0/beatmap.bpm.to_float())
+	metronome.start(60.0/float(beatmap.bpm))
 	print(metronome)
+	acc_timer.start((60.0/float(beatmap.bpm))/float(beatmap.acc))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,6 +30,8 @@ func file_to_array(map_loc_yes):
 	var map_settings = content.split("START")[0].split(";")
 	var bpm
 	var len
+	var acc
+	print(map_settings)
 	for i in map_settings:
 		var settingy_thing = i.split(":")
 		match settingy_thing[0]:
@@ -33,6 +39,8 @@ func file_to_array(map_loc_yes):
 				bpm = settingy_thing[1]
 			"len":
 				len = settingy_thing[1]
+			"acc":
+				acc = settingy_thing[1]
 	var map_body = content.split("START")[1].split(",")
 	for i in map_body:
 		if actions.has(i):
@@ -46,21 +54,23 @@ func file_to_array(map_loc_yes):
 					beat_array.append("R")
 			else:
 				print("your beatmap is broken :(")
-				return {"map":["w", "w", "s", "s", "a", "d", "a", "d", "h", "j"], "bpm": 20, "len": 10}
+				return {"map":["w", "w", "s", "s", "a", "d", "a", "d", "h", "j"], "bpm": 20, "len": 10, "acc":30}
 	print(beat_array)
-	return {"map":beat_array, "bpm": bpm, "len": len}
+	return {"map":beat_array, "bpm": bpm, "len": len, "acc": acc}
 
 
 func _on_metronome_timeout() -> void:
-	check_prev_note()
+	check_notes()
 	if beatmap.map != []:
 		print("VOTE YURI PARTY")
 		var curr_note = beatmap.map[0]
 		beatmap.map.remove_at(0)
+		GlobalData.current_note = curr_note
+		GlobalData.next_note = beatmap.map[0] # since we removed the current note :4
 		if curr_note == "R":
 			pass
 		else:
-			GlobalData.current_note = curr_note
+			
 			var instance = note.instantiate()
 			add_child(instance)
 
@@ -82,7 +92,71 @@ func add_keys_pressed() -> void:
 		curr_clicks.append("j")
 	elif Input.is_action_just_pressed("jump"):
 		curr_clicks.append("h")
-	clicks.append(curr_clicks)
+	current_acc_clicks.append(curr_clicks)
 
-func check_prev_note():
-	pass
+func check_notes():
+	var current_note_found
+	var next_note_found
+	var accs_lost_for_next_note
+	var accs_lost_for_curr_note
+	if (GlobalData.current_note != "R"):
+		current_note_found = false; 
+	else:  
+		current_note_found = true
+		accs_lost_for_curr_note = 0
+	if (GlobalData.next_note != "R"):
+		next_note_found = false; 
+	else:  
+		next_note_found = true # NOTE: U HAVE TO CHECK IF THE next NOTE WAS ALREADY FOUND AT SOME POINT
+		accs_lost_for_next_note = 0
+	for i in range(current_note_clicks.size()):
+		if (current_note_clicks[i].is_empty() != true):
+			for n in range(current_note_clicks[i].size()):
+				if (current_note_clicks[i][n].has(GlobalData.current_note) && current_note_found == false):
+					current_note_found = true
+					accs_lost_for_curr_note = n
+				if (current_note_clicks[i][n].has(GlobalData.next_note) && next_note_found == false):
+					next_note_found = true
+					accs_lost_for_next_note = current_acc_clicks.size() - n
+	if (current_note_found == false):
+		accs_lost_for_curr_note = "M"
+	if (next_note_found == false):
+		accs_lost_for_next_note = "M"
+	print(accs_lost_for_curr_note)
+	all_clicks.append(current_note_clicks)
+	current_note_clicks = []
+
+
+func _on_acc_timeout() -> void:
+	# here we add all the notes ehe and like check yk. so the current_acc_clicks has everything that should be checked here ehe
+	current_note_clicks.append(current_acc_clicks) # adding those! for liek good yes ehe :3
+	current_acc_clicks = []
+	# get the last and next note uwu
+	# i just realized i put these in the wrong place
+	#var current_note_found
+	#var next_note_found
+	#var accs_lost_for_next_note
+	#var accs_lost_for_curr_note
+	#if (GlobalData.current_note != "R"):
+		#current_note_found = false; 
+	#else:  
+		#current_note_found = true
+		#accs_lost_for_curr_note = 0
+	#if (GlobalData.next_note != "R"):
+		#next_note_found = false; 
+	#else:  
+		#next_note_found = true # NOTE: U HAVE TO CHECK IF THE next NOTE WAS ALREADY FOUND AT SOME POINT
+		#accs_lost_for_next_note = 0
+	#for i in range(current_acc_clicks.size()):
+		#if (current_acc_clicks[i].is_empty() != true):
+			#for n in range(current_acc_clicks[i].size()):
+				#if (n == GlobalData.current_note && current_note_found == false):
+					#current_note_found = true
+					#accs_lost_for_curr_note = i
+				#if (n == GlobalData.next_note && next_note_found == false):
+					#next_note_found = true
+					#accs_lost_for_next_note = current_acc_clicks.size() - i
+	#if (current_note_found == false):
+		#accs_lost_for_curr_note = "M"
+	#if (next_note_found == false):
+		#accs_lost_for_next_note = "M"
